8f6f5c7ad58d684246dd8638729ae68bd0be3cec
sandip95-web <sandipmaharjan95@gmail.com>
2025-07-13 18:38:20 +0545

    fix: update image paths to use absolute URLs for consistency

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="../../public/images/me.jpg"
+        src="/images/me.jpg"
         alt="Abdelrahman Qassem"
         className="max-h-[450px] w-auto rounded-full"
       />
